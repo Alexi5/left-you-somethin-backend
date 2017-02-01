b544b4d772af3811bfd918ad914c9741f8bcf7e4
Jean Chung <jxchung@gmail.com>
2017-02-01 11:06:44 -0600

    adding user routes

--- server/user.js
+++ server/user.js
@@ -1,14 +1,23 @@
-// const User = require('../db/Users.js');
+const { User } = require('../db');
 const router = require('express').Router();
 
-module.exports = router
-  .get('/', (req, res, next) => {
-    res.send('Hit Users Page')
-    .catch(next);
-    // User.findAll()
-    // .then( users => {
-    //   res.json(users)
-    // })
-    // .catch(next)
-  });
+router.get('/', (req, res, next) => {
+  res.send('Hit Users Page')
+  .catch(next);
+  // User.findAll()
+  // .then( users => {
+  //   res.json(users)
+  // })
+  // .catch(next)
+});
 
+router.post('/', (req, res, next) => {
+  User.findOrCreate({
+    where: { email: req.body.email },
+    defaults: { name: req.body.displayName },
+  })
+    .then(([user, created]) => res.send(user.id))
+    .catch(next);
+});
+
+module.exports = router;
