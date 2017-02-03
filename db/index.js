3a891787e2246d1604d4c2f52df2316c6345a344
Jean Chung <jxchung@gmail.com>
2017-02-03 14:43:37 -0600

    seedfile fixed!

--- db/index.js
+++ db/index.js
@@ -1,35 +1,10 @@
-// const User = require('./User');
-// const Egg = require('./Egg');
-// const Payload = require('./Payload');
-
-// Egg.belongsTo(User, { as: 'sender' });
-// Egg.belongsTo(User, { as: 'receiver' });
-
-// // Egg.belongsTo(Payload, {as: 'payloadId'});
-// // Payload.belongsTo(Egg, {as: 'payloadId' });
-
-// // sets eggId in payload table
-// // Payload.belongsTo(Egg, {as: 'eggId' });
-// // Egg.hasOne(Payload)
-
-// // sets payload_Id in egg table
-// Egg.belongsTo(Payload, { as: 'payload'});
-// // Payload.belongsTo(Egg);
-
-
-// module.exports = { User, Egg, Payload };
-
-
 const User = require('./User');
 const Egg = require('./Egg');
 const Payload = require('./Payload');
 
-Egg.belongsTo(User, { as: 'sender' });
-Egg.belongsTo(User, { as: 'receiver' });
+Egg.belongsTo(User, { as: 'sender', targetKey: 'fbId' });
+Egg.belongsTo(User, { as: 'receiver', targetKey: 'fbId' });
 
 Egg.belongsTo(Payload);
-
-// Payload.belongsTo(Egg, { as: 'eggId' });
-// Egg.hasOne(Payload)
 
 module.exports = { User, Egg, Payload };
