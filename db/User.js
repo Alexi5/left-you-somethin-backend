334118a8b4de2c75128daca3bf8c623557e4732f
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-02-03 14:31:51 -0600

    modified User in db and api route for user

--- db/User.js
+++ db/User.js
@@ -4,7 +4,7 @@ const db = require('./db');
 const userSchema = {
   fbId: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: true
   },
   firstName: {
     type: Sequelize.STRING,
