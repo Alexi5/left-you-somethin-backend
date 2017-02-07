693116aac602cbf5c30a3411586f9e1c96b5d211
Rebekah Klemm <rebekah_klemm@yahoo.com>
2017-02-07 17:01:53 -0600

    deployment

--- db/db.js
+++ db/db.js
@@ -1,8 +1,12 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/leftyousomethin', {
-    logging: false
-});
+// const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/leftyousomethin', {
+//     logging: false
+// });
+
+// const db = new Sequelize('leftyousomethin', 'hatch', 'hatch', {dialect: 'postgres', port: 5432, logging:true});
+
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/leftyousomethin');
 
 db.authenticate()
     .then(function(err) {
